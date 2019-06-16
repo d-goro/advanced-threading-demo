5e9a67d0ff1c54d40255c476f3bdf720845674bd
Dmitry Gorochovsky <dmitry.gorochovsky@quest.com>
2019-06-16 16:20:17 +0300

    Monitor Wait/Pulse

---
+++ SignalingWaitPulse/PCQueue.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace SignalingWaitPulse
+{
+    /// <summary>
+    /// Producer/Consumer Queue with monitor wait/pulse
+    /// </summary>
+    public class PCQueue
+    {
+        readonly Thread[] _workers;
+        readonly object _locker = new object();
+        readonly Queue<Action> _itemQ = new Queue<Action>();
+
+        public PCQueue (int workerCount)
+        {
+            _workers = new Thread [workerCount];
+ 
+            // Create and start a separate thread for each worker
+            for (var i = 0; i < workerCount; i++)
+            {
+                (_workers [i] = new Thread (Consume)).Start();
+            }
+        }
+
+        public void Shutdown (bool waitForWorkers)
+        {
+            // Enqueue one null item per worker to make each exit.
+            foreach (var worker in _workers)
+            {
+                EnqueueItem (null);
+            }
+            
+            if (waitForWorkers)
+            {// Wait for workers to finish
+                foreach (var worker in _workers)
+                {
+                    worker.Join();
+                }
+            }
+        }
+ 
+        public void EnqueueItem (Action item)
+        {
+            lock (_locker)
+            {
+                _itemQ.Enqueue (item);           // We must pulse because we're
+                Monitor.Pulse (_locker);         // changing a blocking condition.
+            }
+        }
+ 
+        void Consume()
+        {// Keep consuming until told otherwise.
+            while (true)                        
+            {
+                Action item;
+                lock (_locker)
+                {
+                    while (_itemQ.Count == 0) 
+                        Monitor.Wait (_locker);
+
+                    item = _itemQ.Dequeue();
+                }
+                if (item == null)
+                    return;    // This signals our exit.
+
+                item();  // Execute item.
+            }
+        }
+    }
+}
---
+++ SignalingWaitPulse/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+
+namespace SignalingWaitPulse
+{
+    class Program
+    {
+        static readonly object _locker = new object();
+        static bool _go;
+
+        static void Main(string[] args)
+        {
+            new Thread (Work).Start();
+            Console.WriteLine("Press Enter to pulse worker thread");
+            Console.ReadLine();
+ 
+            lock (_locker)
+            {// setting _go=true and pulsing.
+                _go = true;
+                Monitor.Pulse (_locker);
+            }
+
+            Console.WriteLine("Demo of Producer/Consumer Queue with monitor wait/pulse");
+            var q = new PCQueue (2);
+            Console.WriteLine ("Enqueuing 10 items...");
+ 
+            for (int i = 0; i < 10; i++)
+            {
+                int itemNumber = i;      // To avoid the captured variable trap
+                q.EnqueueItem (() =>
+                {
+                    Thread.Sleep (1000);          // Simulate time-consuming work
+                    Console.Write (" Task" + itemNumber);
+                });
+            }
+ 
+            q.Shutdown (true);
+            Console.WriteLine();
+            Console.WriteLine ("Workers complete!");
+        }
+
+        static void Work()
+        {
+            Console.WriteLine("work thread started");
+            lock (_locker)
+            {
+                while (!_go)
+                {
+                    Monitor.Wait (_locker);    // Lock is released while we’re waiting
+                    // lock is regained
+                }
+            }
+            Console.WriteLine ("Woken!!!");
+        }
+    }
+}
